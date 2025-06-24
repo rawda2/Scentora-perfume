79ddbc468c96eceea28c2b13918849b4bca05baa
Rawda Ashor <141156812+rawda2@users.noreply.github.com>
2025-06-24 15:04:32 +0300

    Layout.jsx

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
-import NavBar from '../Navbar/Navbar';
+import NavBar from '../NavBar/NavBar';
 import Footer from '../Footer/Footer';
 
 
